a116d57be44aac348602315395e89a1955bc2319
JulienHatemian <julien-hat.62@hotmail.fr>
2023-06-20 11:54:00 +0200

    Feat correction de fautes et ajout de documentation pour le chatbot sur la page d'accueil

--- chatbot/Views/admininterface.view.php
+++ chatbot/Views/admininterface.view.php
@@ -1,8 +1,32 @@
-<div class="admin-container">
-    <div class="container-text">
-        <label>Enregistrer une Nouvelle réponse </label>
-        <p>Enregistrer une nouvelle réponse puis associée là à des mot clée </p>
-        <textarea  id="response" name="response" placeholder="Nouvelle réponse du bot"></textarea>
-        <button type="submit">Associer mes mots cléee</button>
+<h1>Bienvenue sur l'administration de Billy</h1>
+<div class="instruction">
+    <div class="admin-container instruction-item">
+        <div class="container-text">
+            <h3>Ajouter des mots-clés</h3>
+            <p>Billy vous permet d'ajouter de nouveau mots-clés à rattacher une réponse.</p>
+            <!-- <p>Dans l'onglet "Mots-clés", tapez votre réponse, insérer vos mots-clés et HOP Billy est prêt à l'utilisation !</p> -->
+        </div>
+    </div>
+
+    <div class="admin-container instruction-item">
+        <div class="container-text">
+            <h3>Ajouter de nouveaux produits</h3>
+            <p>Vous pouvez ajouter de nouveaux produits que vos clients pourront retrouver via Billy !</p>
+            <!-- <p>Dans l'onglet "Produit", tapez le nom de votre produit, définissez le prix, sa catégorie, ainsi que sa référence ou encore editer et supprimer !</p> -->
+        </div>
+    </div>
+
+    <div class="admin-container instruction-item">
+        <div class="container-text">
+            <h3>Définissez vos catégories</h3>
+            <p>En définissant vos catégories, vous pourrez les rattacher à vos produits !</p>
+        </div>
+    </div>
+
+    <div class="admin-container instruction-item">
+        <div class="container-text">
+            <h3>Voyez la vie en couleur</h3>
+            <p>Billy propose un choix de couleurs varié pour répondre à vos envies !</p>
+        </div>
     </div>
 </div>
